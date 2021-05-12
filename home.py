5abbfcf31ab2d4dfeb2f818c09d2b00b205e32f7
Timothy Johnson <60270341+Tjohnzon25@users.noreply.github.com>
2021-05-12 14:50:26 -0700

    Added transcript feature to show what .wav file says

--- home.py
+++ home.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
+#Date 5/5/21
 
+from flask import Flask, render_template, request, redirect
+from flask_bootstrap import Bootstrap
+import speech_recognition as sr
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 
-@app.route('/')
-def home():
-    return render_template('home.html')
+@app.route("/", methods=["GET", "POST"])
+def transcript():
+    transcript = ""
+    if request.method == "POST":
+
+        if "file" not in request.files:
+            return redirect(request.url)
+
+        file = request.files["file"]
+        if file.filename == "":
+            return redirect(request.url)
+
+        if file:
+            recognizer = sr.Recognizer()
+            audioFile = sr.AudioFile(file)
+            with audioFile as source:
+                data = recognizer.record(source)
+            transcript = recognizer.recognize_google(data, key=None) #This var holds the sentence
+
+    return render_template('home.html', transcript=transcript)
